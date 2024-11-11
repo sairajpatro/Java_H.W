f56bef6bcff12b3387f1c624426b028c2924d4df
sairajpatro <sairaj.patro@svisnavimumbai.edu.in>
2024-11-11 16:47:03 +0530

    Create 2.ch-8.java

---
+++ 2.ch-8.java
@@ -0,0 +1,13 @@
+//Write a program to display all the natural numbers from 1 to 10.
+class natural_no_loop
+{
+    public static void main()
+    {
+        int a;
+        System.out.println("Natural numbers from 1 to 10");
+        for(a=1;a<=10;a++)
+        {
+            System.out.print(a);
+        }
+    }
+}

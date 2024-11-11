71dc85e3ca609b6413f1be4eccf4381e19e1fb53
sairajpatro <sairaj.patro@svisnavimumbai.edu.in>
2024-11-11 16:41:40 +0530

    Create 1.loop.java

---
+++ 1.loop.java
@@ -0,0 +1,12 @@
+//Write a program to display the message 'Happy New Year 'ten times on the screen
+class HNY_loop
+{
+    public static void main()
+    {
+        int a;
+        for(a=1;a<=10;a++)
+        {
+            System.out.println("Happpy new year");
+        }
+    }
+}
